6a00d9c9db4ade80bb6c4949fdb75780765c9db5
Paul Otten <lightnica@yahoo.ca>
2023-12-06 09:30:33 -0500

    day 6

---
+++ src/challenges/day6.rs
@@ -0,0 +1,24 @@
+use axum::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct ElfCount {
+    elf: usize,
+    #[serde(rename="elf on a shelf")]
+    elf_on_a_shelf: usize,
+    #[serde(rename="shelf with no elf on it")]
+    shelf_with_no_elf: usize,
+}
+
+pub async fn elf(body: String) -> Json<ElfCount> {
+    let elf = body.matches("elf").count();
+    let elf_on_a_shelf = body.matches("elf on a shelf").count();
+    let shelf = body.matches("shelf").count();
+    let shelf_with_no_elf = shelf - elf_on_a_shelf;
+
+    Json(ElfCount {
+        elf,
+        elf_on_a_shelf,
+        shelf_with_no_elf,
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ async fn main() -> shuttle_axum::ShuttleAxum {
         .route("/1/*ids", get(challenges::day1::cube_bits))
         // day 4
         .route("/4/strength", post(challenges::day4::strength))
-        .route("/4/contest", post(challenges::day4::contest));
+        .route("/4/contest", post(challenges::day4::contest))
+        // day 6
+        .route("/6", post(challenges::day6::elf));
 
     Ok(router.into())
 }
